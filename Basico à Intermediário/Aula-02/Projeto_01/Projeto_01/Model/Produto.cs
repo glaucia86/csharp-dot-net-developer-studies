9cd08bba2877254f54b9267c2d17c912da9b8e1e
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-04-02 20:15:38 -0300

    Criação da aula 02

---
+++ Basico à Intermediário/Aula-02/Projeto_01/Projeto_01/Model/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace Projeto_01.Model
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+
+        public string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+    }
+}
