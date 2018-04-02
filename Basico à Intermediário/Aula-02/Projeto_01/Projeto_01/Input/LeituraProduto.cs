d041bdb6683a707fdb2aa86f1fd1f469203e45b2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-04-02 20:20:18 -0300

    Criando a classe de entrada de dados.

---
+++ Basico à Intermediário/Aula-02/Projeto_01/Projeto_01/Input/LeituraProduto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_01.Input
+{
+    public class LeituraProduto
+    {
+        public int LerCodigo()
+        {
+            Console.WriteLine("Informe o Código do Produto: ");
+            int codigo = Convert.ToInt32(Console.ReadLine());
+
+            return codigo;
+        }
+
+        public string LerNomeProduto()
+        {
+            Console.WriteLine("Informe o nome do Produto: ");
+            string nome = Console.ReadLine();
+
+            return nome;
+        }
+
+        public decimal LerPrecoProduto()
+        {
+            Console.WriteLine("Informe o Preço do Produto: R$ ");
+            decimal preco = Convert.ToDecimal(Console.ReadLine());
+
+            return preco;
+        }
+    }
+}
