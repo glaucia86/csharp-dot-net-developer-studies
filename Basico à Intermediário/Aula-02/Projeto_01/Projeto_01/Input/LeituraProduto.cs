4913368c5a507a35973ddcd975f0b618f3c55fa7
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-04-02 20:24:26 -0300

    Criando a execução do app.

--- Basico à Intermediário/Aula-02/Projeto_01/Projeto_01/Input/LeituraProduto.cs
+++ Basico à Intermediário/Aula-02/Projeto_01/Projeto_01/Input/LeituraProduto.cs
@@ -10,7 +10,7 @@ namespace Projeto_01.Input
     {
         public int LerCodigo()
         {
-            Console.WriteLine("Informe o Código do Produto: ");
+            Console.Write("Informe o Código do Produto: ");
             int codigo = Convert.ToInt32(Console.ReadLine());
 
             return codigo;
@@ -18,7 +18,7 @@ namespace Projeto_01.Input
 
         public string LerNomeProduto()
         {
-            Console.WriteLine("Informe o nome do Produto: ");
+            Console.Write("Informe o nome do Produto: ");
             string nome = Console.ReadLine();
 
             return nome;
@@ -26,7 +26,7 @@ namespace Projeto_01.Input
 
         public decimal LerPrecoProduto()
         {
-            Console.WriteLine("Informe o Preço do Produto: R$ ");
+            Console.Write("Informe o Preço do Produto: R$ ");
             decimal preco = Convert.ToDecimal(Console.ReadLine());
 
             return preco;
--- Basico à Intermediário/Aula-02/Projeto_01/Projeto_01/Program.cs
+++ Basico à Intermediário/Aula-02/Projeto_01/Projeto_01/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Projeto_01.Input;
+using Projeto_01.Model;
 
 namespace Projeto_01
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var p = new Produto();
+
+            var lp = new LeituraProduto();
+
+            p.Codigo = lp.LerCodigo();
+            p.Nome = lp.LerNomeProduto();
+            p.Preco = lp.LerPrecoProduto();
+
+            Console.WriteLine("\nDados do Produto");
+            Console.WriteLine("Código..............: " + p.Codigo);
+            Console.WriteLine("Nome................: " + p.Nome);
+            Console.WriteLine("Preço...............: " + p.Preco);
+
+            Console.ReadLine();
         }
     }
 }
