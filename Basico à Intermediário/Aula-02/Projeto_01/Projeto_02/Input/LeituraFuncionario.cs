042d6b7a1b288c6dd35d55638fe11a17ca2d121f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-04-02 21:44:14 -0300

    Realizando exercício de fixação.

---
+++ Basico à Intermediário/Aula-02/Projeto_01/Projeto_02/Input/LeituraFuncionario.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Projeto_02.Input
+{
+    public class LeituraFuncionario
+    {
+        public int LerCodigoFuncionario()
+        {
+            try
+            {
+                int codigoFuncionario = Convert.ToInt32(Console.ReadLine());
+
+                return codigoFuncionario;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+                return LerCodigoFuncionario();
+            }
+        }
+
+        public string LerNomeFuncionario()
+        {
+            try
+            {
+                string nomeFuncionario = Console.ReadLine();
+
+                var regex = new Regex("^[A-Za-zÀ-Üà-ü0-9\\s]{3,30}$");
+
+                if (regex.IsMatch(nomeFuncionario))
+                {
+                    return nomeFuncionario;
+                }
+
+                throw new Exception("Nome inválido!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return LerNomeFuncionario();
+            }
+        }
+
+        public int LerIdadeFuncionario()
+        {
+            try
+            {
+                int idadeFuncionario = Convert.ToInt32(Console.ReadLine());
+
+                return idadeFuncionario;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return LerIdadeFuncionario();
+            }
+        }
+
+        public string LerFuncaoFuncionario()
+        {
+            try
+            {
+                string funcaoFuncionario = Console.ReadLine();
+
+                var regex = new Regex("^[A-Za-zÀ-Üà-ü0-9\\s]{3,30}$");
+
+                if (regex.IsMatch(funcaoFuncionario))
+                {
+                    return funcaoFuncionario;
+                }
+
+                throw new Exception("Função inválido!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+                return LerFuncaoFuncionario();
+            }
+        }
+
+        public int LerTempoEmpresaFuncionario()
+        {
+            try
+            {
+                int tempoEmpresa = Convert.ToInt32(Console.ReadLine());
+
+                return tempoEmpresa;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return LerTempoEmpresaFuncionario();
+            }
+        }
+    }
+}
---
+++ Basico à Intermediário/Aula-02/Projeto_01/Projeto_02/Model/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿namespace Projeto_02.Model
+{
+    public class Funcionario
+    {
+        public int IdFuncionario { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Funcao { get; set; }
+
+        public int TempoEmpresa { get; set; }
+    }
+}
