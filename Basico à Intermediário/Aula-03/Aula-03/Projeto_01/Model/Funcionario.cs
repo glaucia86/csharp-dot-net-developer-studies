b12f32d0302dcdf7ee60f58fb4d3ddd614dfa511
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-04-11 18:48:27 -0300

    Criando classes overloading e override.

--- Basico à Intermediário/Aula-03/Aula-03/Projeto_01/Model/Funcionario.cs
+++ Basico à Intermediário/Aula-03/Aula-03/Projeto_01/Model/Funcionario.cs
@@ -1,6 +1,33 @@
-﻿namespace Projeto_01.Model
+﻿using System;
+
+namespace Projeto_01.Model
 {
     public class Funcionario
     {
+        public int IdFuncionario { get; set; }
+
+        public string Nome { get; set; }
+
+        public decimal Salario { get; set; }
+
+        // Construtor:
+        public Funcionario()
+        {
+            //default
+        }
+
+        // Sobrecarga de Construtores: (Overloading):
+        public Funcionario(int idFuncionario, string nome, decimal salario)
+        {
+            this.IdFuncionario = idFuncionario;
+            this.Nome = nome;
+            this.Salario = salario;
+        }
+
+        // Sobescrita dos métodos da Classe Object:
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2:c}", IdFuncionario, Nome, Salario);
+        }
     }
 }
