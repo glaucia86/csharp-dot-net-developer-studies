254c7b712842a53574051244485ea474971148e5
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-04-11 20:27:09 -0300

    Uso de Interfaces em arquivos 'xml'

--- Basico à Intermediário/Aula-03/Aula-03/Projeto_02/Model/Xml.cs
+++ Basico à Intermediário/Aula-03/Aula-03/Projeto_02/Model/Xml.cs
@@ -1,6 +1,31 @@
-﻿namespace Projeto_02.Model
+﻿using System.IO;
+using System.Text;
+
+namespace Projeto_02.Model
 {
     public class Xml : IArquivo
     {
+        private StreamWriter sw;
+
+        public void AbrirArquivo()
+        {
+            sw = new StreamWriter("C:\\Users\\Glaucia\\Downloads\\Labs\\csharp-dot-net-developer-studies\\Basico à Intermediário\\Aula-03\\Aula-03\\aula.xml");
+        }
+
+        public void GravarConteudo(string texto)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("<?xml version='1.0' ?>");
+            sb.Append("<dados>");
+                sb.AppendFormat("<texto>{0}</texto>");
+            sb.Append("</dados>");
+            sw.WriteLine(sb.ToString());
+        }
+
+        public void FecharArquivo()
+        {
+            sw.Close();
+        }
     }
 }
