f0cb58f65af03a0b3558cb37de6c36e85064706f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-08-03 17:44:29 -0300

    Criacao da classe 'Leitura.cs'

---
+++ Basico à Intermediário/Aula-04/Projeto_01/Projeto_01/Input/Leitura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Projeto_01.Input
+{
+    public class Leitura
+    {
+        // Método responsável por Retornar o Id de 'Pessoa':
+        public int RetornarIdPessoa()
+        {
+            Console.Write("Por favor, informe o seu Id....: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public string RetornarNome()
+        {
+            Console.Write("Por favor, informe o seu nome.....: ");
+            return Console.ReadLine();
+        }
+
+        public string RetornarEmail()
+        {
+            Console.Write("Por favor, informe o seu e-mail.....: ");
+            return Console.ReadLine();
+        }
+    }
+}
